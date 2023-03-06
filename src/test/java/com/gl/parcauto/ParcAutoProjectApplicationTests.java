703afef20328d1ae1b38ba08202c24845917d58e
HichamBouttaj <bouttajhicham@gmail.com>
2023-03-06 22:21:18 +0100

    init project

---
+++ src/test/java/com/gl/parcauto/ParcAutoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gl.parcauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcAutoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
